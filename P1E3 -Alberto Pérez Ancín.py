45416b2a35dceee9187debfb50df4268a3e90db9
Alberto Pérez Ancín <aperez@cifpfbmoll.eu>
2019-10-24 16:38:35 +0200

    Ejercicios de programación

---
+++ P1E3 -Alberto Pérez Ancín.py
@@ -0,0 +1,9 @@
+#P1E3 Alberto Pérez Ancín
+#Diagrama que diga si un número es par o non
+
+n =int (input ("Escribe un número:\n"))
+ 
+if n%2 == 1:
+    print ("Es un número par")
+else:
+    print ("No es un número par")
